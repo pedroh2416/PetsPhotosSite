48ae30f9a3e8302cce4ee2e297616cc38708c9df
Pedro Hernandez <54558045+pedroh2416@users.noreply.github.com>
2021-07-17 17:09:18 -0700

    add new build scripts, update .gitignore
    update and added build scripts, photos directory as well

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.setUseGitIgnore(false);
 };
-return {
-    pathPrefix: "/silly-eleventy-demo/"
-}
+module.exports = {
+    pathPrefix: "/cpsc349-project1"
+};
 
